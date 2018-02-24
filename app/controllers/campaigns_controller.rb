03e4e773bec68cfb3c940860d50c985a50218cb8
Oscar Amado <oaamados@gmail.com>
2018-02-24 18:20:50 -0500

    compare ads and campaigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,4 +1,64 @@
+require 'net/http'
+
 class CampaignsController < ApplicationController
   def sync_ads
+    campaigns = params[:campaigns]
+
+
+    head :bad_request and return unless campaigns
+
+    # call external API
+    result = Net::HTTP.get(URI.parse('http://mockbin.org/bin/fcb30500-7b98-476f-810d-463a0b8fc3df'))
+    ads = result.nil? ? [] : JSON.parse(result)['ads']
+
+    # foreach campaigns compare
+    sync_result = []
+
+    campaigns.each do |campaign|
+
+      sync_status = {'id': campaign['id'],
+                     'job_id': campaign['job_id'],
+                     'status': campaign['status'],
+                     'external_reference': campaign['external_reference'],
+                     'ad_description': campaign['ad_description']}
+
+
+
+      if campaign['external_reference'].nil? || campaign['external_reference'].empty?
+        sync_status['sync_status'] = "Campaign doesn't have external_id"
+        sync_result << sync_status
+        next
+      end
+
+      ad = ads.first { |ad| ad['reference'] == campaign['external_reference'] }
+      if ad.nil? || ad.empty?
+        sync_status['sync_status'] = "Ad doesn't exist"
+      else
+        sync_status['ad_status'] = ad['status']
+        sync_status['ad_description'] = ad['description']
+
+        if (campaign['status'] == 'active' && ad['status'] == 'enabled') ||
+              (campaign['status'] == 'paused' && ad['status'] == 'disabled') ||
+              (campaign['status'] == 'deleted' && ad['status'] == 'disabled')
+          sync_status['sync_status'] = 'Sync'
+        else
+          sync_status['sync_status'] = 'Not Sync'
+        end
+
+        if campaign['ad_description'] == ad['description']
+          sync_status['sync_description'] = 'Sync'
+        else
+          sync_status['sync_description'] = 'Not Sync'
+        end
+
+      end
+
+      sync_result << sync_status
+    end
+
+
+    # return a solution
+    render json: {'campaigns': sync_result}
   end
+
 end
--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -2,11 +2,163 @@ require 'rails_helper'
 
 RSpec.describe CampaignsController, type: :controller do
 
-  describe "GET #sync_ads" do
-    it "returns http success" do
-      get :sync_ads
+  describe 'POST #sync_ads' do
+    it 'returns http success' do
+      post :sync_ads, params: {campaigns: ['foo'], format: :json }
       expect(response).to have_http_status(:success)
     end
+
+    it 'call the ads API' do
+      expect(Net::HTTP).to receive(:get).once
+      post :sync_ads, params: {campaigns: ['foo'], format: :json }
+    end
+
+    it "when ad doesn't exist it returns Ad doesn't exist" do
+      campaign = {'id': 1,
+                  'job_id': 1,
+                  'status': 'active',
+                  'external_reference': 1,
+                  'ad_description': 'foo'}
+      expect(Net::HTTP).to receive(:get).and_return("{\"ads\":[]}")
+
+      post :sync_ads, params: { campaigns: [campaign], format: :json }
+
+      resp = JSON.parse(response.body)
+      expect( resp['campaigns'][0]['sync_status']).to eq("Ad doesn't exist")
+    end
+
+    it "when the campaign doesn't have a external_reference it returns Campaign doesn't have external_id" do
+      campaign = {'id': 1,
+                  'job_id': 1,
+                  'status': 'active',
+                  'external_reference': nil,
+                  'ad_description': 'foo'}
+      expect(Net::HTTP).to receive(:get).and_return("{\"ads\":[]}")
+
+      post :sync_ads, params: { campaigns: [campaign], format: :json }
+
+      resp = JSON.parse(response.body)
+      expect( resp['campaigns'][0]['sync_status']).to eq("Campaign doesn't have external_id")
+
+    end
+
+    it 'when the campaign is active and the ad is enabled returns Sync' do
+      campaign = {'id': 1,
+                  'job_id': 1,
+                  'status': 'active',
+                  'external_reference': '1',
+                  'ad_description': 'foo'}
+      expect(Net::HTTP).to receive(:get).and_return("{\"ads\":[{\"reference\":\"1\",\"status\":\"enabled\"}]}")
+
+      post :sync_ads, params: { campaigns: [campaign], format: :json }
+
+      resp = JSON.parse(response.body)
+      expect( resp['campaigns'][0]['sync_status']).to eq('Sync')
+    end
+
+    it 'when the campaign is active and the ad is disabled returns Not Sync' do
+      campaign = {'id': 1,
+                  'job_id': 1,
+                  'status': 'active',
+                  'external_reference': '1',
+                  'ad_description': 'foo'}
+      expect(Net::HTTP).to receive(:get).and_return("{\"ads\":[{\"reference\":\"1\",\"status\":\"disabled\"}]}")
+
+      post :sync_ads, params: { campaigns: [campaign], format: :json }
+
+      resp = JSON.parse(response.body)
+      expect( resp['campaigns'][0]['sync_status']).to eq('Not Sync')
+    end
+
+    it 'when the campaign is paused and the ad is enabled returns Not Sync' do
+      campaign = {'id': 1,
+                  'job_id': 1,
+                  'status': 'paused',
+                  'external_reference': '1',
+                  'ad_description': 'foo'}
+      expect(Net::HTTP).to receive(:get).and_return("{\"ads\":[{\"reference\":\"1\",\"status\":\"enabled\"}]}")
+
+      post :sync_ads, params: { campaigns: [campaign], format: :json }
+
+      resp = JSON.parse(response.body)
+      expect( resp['campaigns'][0]['sync_status']).to eq('Not Sync')
+    end
+
+
+    it 'when the campaign is paused and the ad is disabled returns Sync' do
+      campaign = {'id': 1,
+                  'job_id': 1,
+                  'status': 'paused',
+                  'external_reference': '1',
+                  'ad_description': 'foo'}
+      expect(Net::HTTP).to receive(:get).and_return("{\"ads\":[{\"reference\":\"1\",\"status\":\"disabled\"}]}")
+
+      post :sync_ads, params: { campaigns: [campaign], format: :json }
+
+      resp = JSON.parse(response.body)
+      expect( resp['campaigns'][0]['sync_status']).to eq('Sync')
+    end
+
+    it 'when the campaign is deleted and the ad is enabled returns Not Sync' do
+      campaign = {'id': 1,
+                  'job_id': 1,
+                  'status': 'deleted',
+                  'external_reference': '1',
+                  'ad_description': 'foo'}
+      expect(Net::HTTP).to receive(:get).and_return("{\"ads\":[{\"reference\":\"1\",\"status\":\"enabled\"}]}")
+
+      post :sync_ads, params: { campaigns: [campaign], format: :json }
+
+      resp = JSON.parse(response.body)
+      expect( resp['campaigns'][0]['sync_status']).to eq('Not Sync')
+    end
+
+    it 'when the campaign is deleted and the ad is disabled returns Sync' do
+      campaign = {'id': 1,
+                  'job_id': 1,
+                  'status': 'deleted',
+                  'external_reference': '1',
+                  'ad_description': 'foo'}
+      expect(Net::HTTP).to receive(:get).and_return("{\"ads\":[{\"reference\":\"1\",\"status\":\"disabled\"}]}")
+
+      post :sync_ads, params: { campaigns: [campaign], format: :json }
+
+      resp = JSON.parse(response.body)
+      expect( resp['campaigns'][0]['sync_status']).to eq('Sync')
+    end
+
+    it 'when the description is synchronized returns sync' do
+      campaign = {'id': 1,
+                  'job_id': 1,
+                  'status': 'deleted',
+                  'external_reference': '1',
+                  'ad_description': 'foo'}
+      expect(Net::HTTP).to receive(:get).and_return("{\"ads\":[{\"reference\":\"1\",\"status\":\"disabled\",
+                                                      \"description\":\"foo\"}]}")
+
+      post :sync_ads, params: { campaigns: [campaign], format: :json }
+
+      resp = JSON.parse(response.body)
+      expect( resp['campaigns'][0]['sync_description']).to eq('Sync')
+    end
+
+
+    it 'when the description is desynchronized returns not sync' do
+      campaign = {'id': 1,
+                  'job_id': 1,
+                  'status': 'deleted',
+                  'external_reference': '1',
+                  'ad_description': 'foo2'}
+      expect(Net::HTTP).to receive(:get).and_return("{\"ads\":[{\"reference\":\"1\",\"status\":\"disabled\",
+                                                      \"description\":\"foo\"}]}")
+
+      post :sync_ads, params: { campaigns: [campaign], format: :json }
+
+      resp = JSON.parse(response.body)
+      expect( resp['campaigns'][0]['sync_description']).to eq('Not Sync')
+    end
+
+
   end
 
 end
